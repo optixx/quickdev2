a4c2532a934ba4720af745de45d7aeda967db65f
optixx <david@optixx.org>
2010-05-22 12:14:13 +0200

    cleanup

--- cpld/hdl/system.v
+++ cpld/hdl/system.v
@@ -1,19 +1,19 @@
 module system (
 
-    inout [7:0] sram_data,
-    output [20:0] sram_addr,
-    output sram_oe_n,
-    output sram_we_n,
-    output sram_ce_n,
+    inout   [7:0]   sram_data,
+    output  [20:0]  sram_addr,
+    output          sram_oe_n,
+    output          sram_we_n,
+    output          sram_ce_n,
 
-    inout [7:0] avr_data,
-    input [2:0] avr_ctrl,
+    inout   [7:0]   avr_data,
+    input   [2:0]   avr_ctrl,
 
-    input avr_ce,
-    input avr_we,
-    input avr_oe,
-    input avr_si,
-    input avr_clk
+    input           avr_ce,
+    input           avr_we,
+    input           avr_oe,
+    input           avr_si,
+    input           avr_clk
 );
     
 //assign sram_oe_n = avr_oe;
